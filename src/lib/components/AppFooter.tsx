fd3d46e6a0ad55dde9b3984fe7ea35a56e577604
RizaMamedow <riza.mamedow22@gmail.com>
2026-02-09 17:06:20 +0500

    feat: add footer links

--- src/lib/components/AppFooter.tsx
+++ src/lib/components/AppFooter.tsx
@@ -7,34 +7,37 @@ import { useAppStore } from "@/src/lib/data/stores/app.store";
 import LoadingText from "@/src/lib/components/common/LoadingText";
 import ContactItem from "@/src/lib/components/common/ContactItem";
 import ErrorText from "@/src/lib/components/common/ErrorText";
-
+import Link from "next/link";
 
 function AppFooter() {
-    const store = useAppStore((s) => s.contacts )
-    const { data, loading, fetch, error } = store
+    const store = useAppStore((s) => s.contacts);
+    const { data, loading, fetch, error } = store;
 
     return (
-        <footer id="app-footer" className={clsx(
-            "border-t mt-20"
-        )}>
-            <Container className={clsx(
-                "w-full flex justify-between  items-center p-2 flex-col",
-                "md:flex-row md:gap-4 gap-3 md:p-5"
-            )}>
+        <footer id="app-footer" className={clsx("border-t mt-20")}>
+            <Container
+                className={clsx(
+                    "w-full flex justify-between  items-center p-2 flex-col",
+                    "md:flex-row md:gap-4 gap-3 md:p-5",
+                )}
+            >
                 <div>
                     <DataWrapper
                         data={data}
                         loading={loading}
                         error={error}
                         fetch={fetch}
-                        loadingComponent={(
-                            <div> 
+                        loadingComponent={
+                            <div>
                                 <LoadingText className="text-base" />
-                            </div>    
-                        )}
+                            </div>
+                        }
                         errorComponent={(error) => (
                             <div>
-                                <ErrorText message={error} className="text-base" />
+                                <ErrorText
+                                    message={error}
+                                    className="text-base"
+                                />
                             </div>
                         )}
                     >
@@ -48,9 +51,19 @@ function AppFooter() {
                     </DataWrapper>
                 </div>
 
-                <div>
+                <div className="text-center md:text-right">
                     <h5>© 2026 Riza Mamedow.</h5>
-                    <h5>Built with ❤️ and open source.</h5>
+                    <h5>
+                        Built with ❤️ and {" "}
+                        <Link
+                            href="https://github.com/RizaMamedow/rizamamedow.github.io"
+                            className="hover:underline"
+                            target="_blank"
+                        >
+                            open source
+                        </Link>
+                        .
+                    </h5>
                 </div>
             </Container>
         </footer>
